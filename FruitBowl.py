ca31753fbada5dd3addca3e3659cb3bd1607a577
trooves101 <ryanpkostka@icloud.com>
2023-02-05 13:31:40 -0600

    yes yes looks cooler

--- FruitBowl.py
+++ FruitBowl.py
@@ -1,15 +1,14 @@
 fruitbowl = []
-storage = input("How much storage does your fruit bowl have?")
+storage = int(input("How much storage does your fruit bowl have?"))
 
 
 def remove():  # creates a function that removes something from the list
     usr1 = input("Which fruit would you like removed?")
-    if fruitbowl.index(usr1) == -1:  # checks if the user input is actually in the list
-        return "Please enter a valid fruit."
-        remove()
-    else:
-        fruitbowl.remove(usr1)  # removes it from the list
+    try:
+        fruitbowl.remove(usr1)
         return fruitbowl
+    except ValueError:
+        return "Please enter a valid fruit."
 
 
 def add():  # function that adds/appends the fruit the user inputs
@@ -23,11 +22,15 @@ def fcount():  # function that counts the amount of fruit the user inputs
     return fruitbowl.count(usr3)
 
 
-while storage != str(len(fruitbowl)):  # while loop that continues until storage == length
+actions = {  # creates a dictionary to store da functions
+    "remove": remove,
+    "add": add,
+    "count": fcount
+}
+
+while storage != len(fruitbowl):  # while loop that continues until storage == length
     usr4 = input("Would you like to remove, add, or count a fruit?")
-    if usr4 == "remove":
-        print(remove())
-    elif usr4 == "add":
-        print(add())
-    elif usr4 == "count":
-        print(fcount())
+    if usr4 in actions:  # checks if the input is in the actions dictionary
+        print(actions[usr4]())
+    else:
+        print("Invalid input, enter 'remove', 'add', or 'count'.")
