fe2e9ed4c58e0d1100b0aee75492e96970f9f2cd
Ryan <ryanpkostka@icloud.com>
2023-02-06 10:12:15 -0600

    Create progress.py
    Just the progress that I've had while learning.

---
+++ progress.py
@@ -0,0 +1,23 @@
+# 1/27/23 - The whole period I was just installing IntelliJ, but I have to update my computer so I won’t be able to make progress for about a day.
+################################################################################################################################################
+################################################################################################################################################
+# 2/3/23 - While we didn’t have school I still worked on the project at home, and I managed to go through 4 chapters of Python on the official Python docs.
+# A significant challenge was understanding some of the differences between Python and Java I really struggled to get used to not using parentheses for 
+# things like if statements. A concept that was difficult to grasp was for loops that were formatted like the example below the main reason this was a 
+# challenge is that I was confused at how it looped through the variables, but eventually, I figured out that it’s just 2 loops without taking up more space 
+# this could be used for combining 2 lists using zip. I’m enjoying Python a lot more than Java since everything is a lot faster and shorter to write, also 
+# I feel like I’m learning at a much faster rate than I did with Java mainly because I put more effort into it.
+
+Fruit = ["Banana", "Peach", "Apple", "Grape"]
+Color = ["Yellow", "Pink", "Red", "Purple"]
+
+for i, v in zip(Fruit, Color):
+   print(i + " -> " + v)
+
+# Banana Yellow
+# Peach Pink
+# Apple Red
+# Grape Purple
+################################################################################################################################################
+################################################################################################################################################
+# 2/10/23 - 
